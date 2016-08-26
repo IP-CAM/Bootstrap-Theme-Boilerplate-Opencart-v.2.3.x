f6de97d5980e6f1cf2add337b135f0be8fc156d3
almostninja <szymon.kabelis@yahoo.com>
2016-08-26 16:11:06 +0200

    Gulp load plugins

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,7 @@
  * -p - production options
  */
 var gulp         = require('gulp');
-var sass         = require('gulp-sass');
-var concat       = require('gulp-concat');
-var uglify       = require('gulp-uglify');
-var imagemin     = require('gulp-imagemin');
-var sourcemaps   = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
+var $            = require('gulp-load-plugins')();
 var del          = require('del');
 var browserSync  = require('browser-sync').create();
 var runSequence  = require('run-sequence');
@@ -54,10 +49,10 @@ var autoprefixerOptions = config.autoprefixerOptions;
 gulp.task('styles', function() {
     return gulp
         .src(paths.base + paths.styles.source)
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(sourcemaps.write())
-        .pipe(autoprefixer(autoprefixerOptions))
+        .pipe($.sourcemaps.init())
+        .pipe($.sass().on('error', $.sass.logError))
+        .pipe($.sourcemaps.write())
+        .pipe($.autoprefixer(autoprefixerOptions))
         .pipe(gulp.dest(paths.dist + paths.styles.dist))
         .pipe(browserSync.stream());    
 });
@@ -68,10 +63,10 @@ gulp.task('styles', function() {
 gulp.task('scripts', function() {
     return gulp
         .src(paths.base + paths.scripts.source)
-        .pipe(sourcemaps.init())
-        .pipe(concat('app.js'))
-        .pipe(uglify())
-        .pipe(sourcemaps.write())
+        .pipe($.sourcemaps.init())
+        .pipe($.concat('app.js'))
+        .pipe($.uglify())
+        .pipe($.sourcemaps.write())
         .pipe(gulp.dest(paths.dist + paths.scripts.dist))
         .pipe(browserSync.stream());
 });
@@ -81,7 +76,7 @@ gulp.task('scripts', function() {
  */
 gulp.task('images', function() {
     return gulp.src(paths.base + paths.images.source)
-        .pipe(imagemin({
+        .pipe($.imagemin({
             progressive: true,
             interlaced: true,
             svgoPlugins: [{removeUnknownsAndDefaults: false}, {cleanupIDs: false}]
